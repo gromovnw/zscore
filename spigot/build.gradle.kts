b2880956fee25858dde113ae58a2b8b4bdb4d743
zDonate Admin <admin@zertix.pw>
2026-09-19 19:25:43 +0000

    Relocate MySQL driver, trim SQLite natives

--- spigot/build.gradle.kts
+++ spigot/build.gradle.kts
@@ -34,6 +34,8 @@ tasks.shadowJar {
     relocate("eu.okaeri", "it.gromov.zscore.shaded.eu.okaeri")
     relocate("org.yaml.snakeyaml", "it.gromov.zscore.shaded.org.yaml.snakeyaml")
     relocate("com.google.gson", "it.gromov.zscore.shaded.com.google.gson")
+    relocate("com.mysql", "it.gromov.zscore.shaded.com.mysql")
+    exclude("org/sqlite/native/FreeBSD/**", "org/sqlite/native/Linux-Android/**", "org/sqlite/native/Linux/riscv64/**", "org/sqlite/native/Linux/ppc64/**", "org/sqlite/native/Linux/armv6/**", "org/sqlite/native/Linux/armv7/**", "org/sqlite/native/Linux/x86/**", "org/sqlite/native/Windows/armv7/**", "org/sqlite/native/Windows/x86/**")
 }
 
 tasks.build {
